8c8101ed9ee1e19ac77813e6c099ad4a14990481
shilangyu <xmarcinmarcin@gmail.com>
2020-01-09 10:28:59 +0100

    fixed docs

--- docs/example.js
+++ docs/example.js
@@ -1,37 +1,48 @@
-const useAsync = module.exports; // this is done only for the example, usually you'd do: `import useAsync from 'use-async-react'`
+// this is done only for the example, usually you'd do: `import useAsync from 'use-async-react'`
+const useAsync = module.exports;
 
 const Form = () => {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const { call: login, error, loading, result } = useAsync(
-    () =>
+    (username, password) =>
       new Promise((resolve, reject) => {
         setTimeout(() => {
-          Math.random() < 0.7
-            ? resolve("iAmAToken")
-            : reject(new Error("failed to log in"));
+          if (username.length === 0) reject(new Error("username too short"));
+          else if (password.length < 8) reject(new Error("password too short"));
+          else resolve("iAmAToken");
         }, 1000);
       })
   );
 
   return (
-    <div>
+    <div className="row">
       <input
+        className="s5 input-field col"
+        placeholder="username"
         type="text"
         value={username}
         onChange={e => setUsername(e.target.value)}
       />
       <input
+        className="s5 input-field col"
+        placeholder="password"
         type="password"
         value={password}
         onChange={e => setPassword(e.target.value)}
       />
-      <button onClick={login}>Log in</button>
-      <div>
+      <br />
+      <button
+        className="waves-effect waves-light btn-small"
+        onClick={() => login(username, password)}
+      >
+        Log in
+      </button>
+      <p>
         {error && `Error! - ${error}`}
         {loading && "Logging in..."}
-        {result && `Logged in! Here is your token: ${result}`}
-      </div>
+        {result && `Logged in! Here is your token: '${result}'`}
+      </p>
     </div>
   );
 };
